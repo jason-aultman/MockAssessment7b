9f360ee86594ca9b1a2fc96d387d66cd89aa450d
Jason Aultman <jason.aultman@gmail.com>
2020-11-13 20:50:58 -0500

    initial commit

---
+++ MockAssessment7b/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MockAssessment7b.Models;
+
+namespace MockAssessment7b.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
+        private JsonSerializerOptions _jsonSerializerOptions;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            HttpClient httpClient = new HttpClient();
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://grandcircusco.github.io/demo-apis/");
+            _jsonSerializerOptions = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Search(int Id)
+        {
+            var response =  await _httpClient.GetAsync($"donuts/{3}.json");
+            var responseToJson = response.Content.ReadAsStringAsync().GetAwaiter().GetResult(); 
+            var donutResult = JsonSerializer.Deserialize<Donut>(responseToJson,_jsonSerializerOptions);
+            return View(donutResult);
+            
+        }
+
+       
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MockAssessment7b/Models/Donut.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockAssessment7b.Models
+{
+    public class Donut
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public string Photo { get; set; }
+        public List<string> Extras { get; set; }
+
+    }
+}
